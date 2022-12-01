03ca0c07a7efdeaf60fa11fc67f816233e92f5f9
khlee97 <khlee97@sicc.co.kr>
2022-12-01 07:03:17 +0000

    commit

--- customer-center/src/main/java/labshopcqrs/infra/MyPageRepository.java
+++ customer-center/src/main/java/labshopcqrs/infra/MyPageRepository.java
@@ -4,9 +4,12 @@ import labshopcqrs.domain.*;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import java.util.List;
+import java.util.Optional;
 
 @RepositoryRestResource(collectionResourceRel="myPages", path="myPages")
 public interface MyPageRepository extends PagingAndSortingRepository<MyPage, Long> {
+
+    Optional<MyPage> findByOrderId(Long orderId);
 
     
 
